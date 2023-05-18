47310d3a5fb4c02f987cb02febb456b8441c33e2
dafabulousteach <dafabulousteach@gmail.com>
2023-05-17 21:28:33 -0700

    updates link to GUSD adds title and favicon

--- src/root.tsx
+++ src/root.tsx
@@ -18,6 +18,7 @@ export default function Root() {
   return (
     <Html lang="en">
       <Head>
+       <link rel="icon" type="image/x-icon" href="/public/favicon.ico"/>
         <Title>SolidStart - Bare</Title>
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -25,8 +26,8 @@ export default function Root() {
       <Body>
         <Suspense>
           <ErrorBoundary>
-            <A href="/">Index</A>
-            <A href="/about">About</A>
+            {/* <A href="/">Index</A>
+            <A href="/about">About</A> */}
             <Routes>
               <FileRoutes />
             </Routes>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,18 +1,15 @@
 import { Title } from "solid-start";
-import Counter from "~/components/Counter";
 
 export default function Home() {
   return (
     <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
-      <Counter />
+      <Title>GUSD</Title>
+      <h1>We love our LGBTQIA+ students</h1>
       <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
+        Visit the 
+        <a href="https://www.gusd.net/Page/15" 
+        target="_blank">Glendale Unified School District </a>
+        to voice your support for our LGBTQIA+ students.
       </p>
     </main>
   );
